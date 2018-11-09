54c8ab283d8778ea73977e1a87468ddb47569d75
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2018-11-08 22:12:22 -0500

    hola

--- src/main/java/pe/org/sehs/controller/Homecontroller.java
+++ src/main/java/pe/org/sehs/controller/Homecontroller.java
@@ -3,7 +3,7 @@ package pe.org.sehs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+// hola mundo XD
 @Controller
 public class Homecontroller {
 @GetMapping("/")
